6e318d0c00845cd7aa0dbe41ee47bde2401d0886
Ryan Hatfield <12716395+ryanhatfield@users.noreply.github.com>
2025-02-07 20:37:21 +0000

    Add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import { ColorModeButton } from "@/components/ui/color-mode";
-import { Button, Center, HStack } from "@chakra-ui/react";
+"use client";
+import { ColorModeButton, useColorModeValue } from "@/components/ui/color-mode";
+import { Box, Button, Card, Center, HStack, VStack } from "@chakra-ui/react";
 
 export default function Home() {
+  const blue = useColorModeValue("lightBlue", "blue");
+  const off = useColorModeValue("#f9f9f9", "");
   return (
-    <Center>
-      <HStack padding={"1em"}>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-        <ColorModeButton></ColorModeButton>
-      </HStack>
+    <Center backgroundColor={off} minHeight={"vh"}>
+      <VStack>
+        <HStack padding={"1em"} minWidth={"100%"}>
+          <Button>Click me</Button>
+          <Button>Click me</Button>
+          <ColorModeButton
+            marginLeft={"auto"}
+            marginRight={"0"}
+          ></ColorModeButton>
+        </HStack>
+        <Card.Root>
+          <Card.Body>
+            <Box width={"20em"} height={"20em"} backgroundColor={blue} />
+          </Card.Body>
+        </Card.Root>
+      </VStack>
     </Center>
   );
 }
