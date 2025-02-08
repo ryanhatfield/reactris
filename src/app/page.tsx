b5348e74ffc571105227c59ad8e578ef55210f98
Ryan Hatfield <12716395+ryanhatfield@users.noreply.github.com>
2025-02-08 15:53:33 +0000

    Change to client only component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,54 @@
-"use client";
 import { ColorModeButton, useColorModeValue } from "@/components/ui/color-mode";
-import { Box, Button, Card, Center, HStack, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  Center,
+  ClientOnly,
+  HStack,
+  IconButton,
+  VStack,
+} from "@chakra-ui/react";
+import { LuMoveDown, LuMoveLeft, LuMoveRight } from "react-icons/lu";
+
+function Inputs() {
+  return (
+    <Card.Root>
+      <Card.Body>
+        <VStack>
+          <HStack>
+            <IconButton color={"white"}>
+              <LuMoveLeft />
+            </IconButton>
+            <IconButton>
+              <LuMoveDown />
+            </IconButton>
+            <IconButton>
+              <LuMoveRight />
+            </IconButton>
+          </HStack>
+        </VStack>
+      </Card.Body>
+    </Card.Root>
+  );
+}
 
 export default function Home() {
-  const blue = useColorModeValue("lightBlue", "blue");
   const off = useColorModeValue("#f9f9f9", "");
   return (
-    <Center backgroundColor={off} minHeight={"vh"}>
-      <VStack>
-        <HStack padding={"1em"} minWidth={"100%"}>
-          <Button>Click me</Button>
-          <Button>Click me</Button>
-          <ColorModeButton
-            marginLeft={"auto"}
-            marginRight={"0"}
-          ></ColorModeButton>
-        </HStack>
-        <Card.Root>
-          <Card.Body>
-            <Box width={"20em"} height={"20em"} backgroundColor={blue} />
-          </Card.Body>
-        </Card.Root>
-      </VStack>
-    </Center>
+    <ClientOnly>
+      <Center colorPalette={"blue"} bgColor={off} minH={"vh"}>
+        <VStack>
+          <HStack minWidth={"100%"} alignItems={"end"}>
+            <ColorModeButton />
+          </HStack>
+          <Card.Root>
+            <Card.Body>
+              <Box height={"30em"} width={"20em"} as="canvas" />
+            </Card.Body>
+          </Card.Root>
+          <Inputs />
+        </VStack>
+      </Center>
+    </ClientOnly>
   );
 }
