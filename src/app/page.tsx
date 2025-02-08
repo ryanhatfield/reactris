de38dfd332d7a17ccc82c3422d76f5352ca4aa49
Ryan Hatfield <12716395+ryanhatfield@users.noreply.github.com>
2025-02-08 16:47:21 +0000

    Add heading, flex, and types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import {
   Box,
   Card,
   Center,
+  Heading,
   HStack,
   IconButton,
   VStack,
+  Flex,
 } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
 import { LuMoveDown, LuMoveLeft, LuMoveRight } from "react-icons/lu";
 
 function Inputs() {
@@ -36,15 +39,43 @@ function InputCard() {
   );
 }
 
-function GameCanvas() {
-  return <Box height={"30em"} width={"20em"} as="canvas" />;
+enum Block {
+  I = "I",
+  J = "J",
+  L = "L",
+  O = "O",
+  S = "S",
+  T = "T",
+  Z = "Z",
 }
 
-function GameCard() {
+enum EmptyCell {
+  Empty = "Empty",
+}
+
+interface CellOptions {
+  block: Block | EmptyCell;
+}
+
+interface BoardProps {
+  currentBoard: CellOptions[][];
+}
+
+function Board(props: BoardProps) {
+  return (
+    <Flex>
+      <Box />
+      <Box />
+      <Box />
+    </Flex>
+  );
+}
+
+function BoardCard() {
   return (
     <Card.Root>
       <Card.Body>
-        <GameCanvas />
+        <Board />
       </Card.Body>
     </Card.Root>
   );
@@ -53,12 +84,13 @@ function GameCard() {
 export default function Home() {
   const off = useColorModeValue("#f9f9f9", "");
   return (
-    <Center colorPalette={"blue"} bgColor={off} minH={"vh"}>
+    <Center colorPalette={"blue"} bgColor={off} minH={"vh"} paddingY={"0.5em"}>
       <VStack>
-        <HStack minWidth={"100%"} alignItems={"end"}>
+        <HStack minW={"100%"}>
+          <Heading marginRight={"auto"}>Reactris</Heading>
           <ColorModeButton />
         </HStack>
-        <GameCard />
+        <BoardCard />
         <InputCard />
       </VStack>
     </Center>
